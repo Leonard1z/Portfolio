f5e69c0af0caf2fa6a3a7519faf1cb19b0fa785d
Leonard1z <leonardzab0@gmail.com>
2022-11-11 17:46:17 +0100

    Using DbContext Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Portfolio.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
