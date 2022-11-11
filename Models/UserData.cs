c79a67955656b520b534a9fd6f9fd8085914d880
Leonard1z <leonardzab0@gmail.com>
2022-11-11 17:14:57 +0100

    UserData Model

---
+++ Models/UserData.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioPractice2.Models
+{
+    public class UserData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Work { get; set; }
+        public string Description { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
