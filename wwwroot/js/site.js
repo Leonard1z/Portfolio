9167d1e7f44bcd6b282a29ece7b814b639f89720
Leonard1z <leonardzab0@gmail.com>
2022-11-11 18:45:46 +0100

    Making Responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Navbar
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-// Write your JavaScript code.
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
